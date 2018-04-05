1353b6feb3e73658dc69fb9884d81889ac1f0486
tkacleonid <tkacleonid@ya.ru>
2018-04-05 21:44:24 +0300

    Add simple servlet

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,19 +3,22 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
+import servlets.DemoServlet;
 
 
 public class Main {
     public static void main(String [] args) throws Exception {
 
-        //Frontend frontend = new Frontend()
+        DemoServlet frontend = new DemoServlet();
 
         Server server = new Server(8080);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
-        context.addServlet(new ServletHolder(),"/a");
+        context.addServlet(new ServletHolder(frontend),"/*");
+
+        server.start();
+        server.join();
 
 
     }
