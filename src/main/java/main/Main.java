f3efc0ceffaa153ecc4ac492ac34387f5ef70be6
tkacleonid <tkacleonid@ya.ru>
2018-04-05 03:33:36 +0300

    Add main

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+
+
+public class Main {
+    public static void main(String [] args) throws Exception {
+
+        //Frontend frontend = new Frontend()
+
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(),"/a");
+
+
+    }
+}
